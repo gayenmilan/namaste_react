61942babb4fb59ab884da83937575144ddb5c687
gayenmilan <milangayenbuie@gmail.com>
2024-01-09 16:46:18 +0530

    react-shimmer-effects install and implementation

--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -1,86 +1,8 @@
+import { ShimmerPostList } from "react-shimmer-effects";
+
 const Shimmer = () => {
     return(
-        <div className="container-fluid shimmer_ui">
-            <div className="row">
-                <div className="col-2 p-2">
-                    <div className="rest_card">
-                        <div className="image_wrapper">
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ub35uaf2iferroprejgy" className="card_imagestyle"/>
-                        </div>
-                        <h5>Aminia Restaurant</h5>
-                        <p>Biryani, Mughlai, Indian, Kebabs, Tandoor</p>
-                        <p>4.5</p>
-                        <p>34 mins</p>
-                        <p>₹500 for two</p>
-                    </div>
-                </div>
-
-                <div className="col-2 p-2">
-                    <div className="rest_card">
-                        <div className="image_wrapper">
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ub35uaf2iferroprejgy" className="card_imagestyle"/>
-                        </div>
-                        <h5>Aminia Restaurant</h5>
-                        <p>Biryani, Mughlai, Indian, Kebabs, Tandoor</p>
-                        <p>4.5</p>
-                        <p>34 mins</p>
-                        <p>₹500 for two</p>
-                    </div>
-                </div>
-
-                <div className="col-2 p-2">
-                    <div className="rest_card">
-                        <div className="image_wrapper">
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ub35uaf2iferroprejgy" className="card_imagestyle"/>
-                        </div>
-                        <h5>Aminia Restaurant</h5>
-                        <p>Biryani, Mughlai, Indian, Kebabs, Tandoor</p>
-                        <p>4.5</p>
-                        <p>34 mins</p>
-                        <p>₹500 for two</p>
-                    </div>
-                </div>
-
-                <div className="col-2 p-2">
-                    <div className="rest_card">
-                        <div className="image_wrapper">
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ub35uaf2iferroprejgy" className="card_imagestyle"/>
-                        </div>
-                        <h5>Aminia Restaurant</h5>
-                        <p>Biryani, Mughlai, Indian, Kebabs, Tandoor</p>
-                        <p>4.5</p>
-                        <p>34 mins</p>
-                        <p>₹500 for two</p>
-                    </div>
-                </div>
-
-                <div className="col-2 p-2">
-                    <div className="rest_card">
-                        <div className="image_wrapper">
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ub35uaf2iferroprejgy" className="card_imagestyle"/>
-                        </div>
-                        <h5>Aminia Restaurant</h5>
-                        <p>Biryani, Mughlai, Indian, Kebabs, Tandoor</p>
-                        <p>4.5</p>
-                        <p>34 mins</p>
-                        <p>₹500 for two</p>
-                    </div>
-                </div>
-
-                <div className="col-2 p-2">
-                    <div className="rest_card">
-                        <div className="image_wrapper">
-                            <img src="https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/ub35uaf2iferroprejgy" className="card_imagestyle"/>
-                        </div>
-                        <h5>Aminia Restaurant</h5>
-                        <p>Biryani, Mughlai, Indian, Kebabs, Tandoor</p>
-                        <p>4.5</p>
-                        <p>34 mins</p>
-                        <p>₹500 for two</p>
-                    </div>
-                </div>
-            </div>
-        </div>
+        <ShimmerPostList postStyle="STYLE_ONE" col={4} row={2} gap={20} />
     );
 };
 
